063aaefce866d93b2b54242006cbfc89aab3881a
ttello <ttello@adquem.com>
2018-01-02 21:05:02 -0600

    add some attrs to main div

--- src/index/index.kt
+++ src/index/index.kt
@@ -1,22 +1,37 @@
 package index
 
-import react.dom.b
-import react.dom.div
-import react.dom.h1
-import react.dom.render
+import kotlinext.js.js
+import kotlinx.html.style
+import kotlinx.html.title
+import react.dom.*
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
 
     val rootDiv = document.getElementById("root")
         render(rootDiv) {
-            h1 {
-                +"Black Jack"
+            div {
+                h1 { +"Black Jack"}
+
                 div {
-                    div { b { +"Player Hand" } }
-                    div { b { +"cards goes here" } }
-                    div { b { + "12 Points" } }
+                    attrs.title = "Foo"
+                    attrs.style = js{
+                        display = "flex"
+                    }
+
+                    div {
+                        div { b { +"Player Hand" } }
+                        div { b { +"cards goes here" } }
+                        div { b { +"12 Points" } }
+                    }
+
+                    div {
+                        div { b { +"Dealer Hand" } }
+                        div { b { +"cards goes here" } }
+                        div { b { +"14 Points" } }
+                    }
                 }
+
             }
     }
 }
