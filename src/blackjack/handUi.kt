a54b1671b8fd073b9f6ecb645b0513811f2052c5
Jose Luis Cadena <joseluis.cadena@schibsted.com.mx>
2018-01-04 16:49:50 -0600

    initialize render with Game object

--- src/blackjack/handUi.kt
+++ src/blackjack/handUi.kt
@@ -1,13 +1,18 @@
 package blackjack
 
+import blackjack.models.Hand
 import react.RBuilder
 import react.dom.b
 import react.dom.div
 
-fun RBuilder.handUi() {
+fun RBuilder.handUi(hand: Hand) {
     div {
         div { b { +"Player Hand" } }
-        div { b { +"cards goes here" } }
-        div { b { +"12 Points" } }
+        div { b {
+            hand.cards.forEach {
+                div { + it.toString() }
+            }
+        } }
+        div { b { +"${hand.handValue()} Points" } }
     }
 }
--- src/index/index.kt
+++ src/index/index.kt
@@ -2,14 +2,17 @@
 package index
 
 import blackjack.gameUi
+import blackjack.models.Game
 import react.dom.render
 import kotlin.browser.document
+
+val game = Game()
 
 fun main(args: Array<String>) {
 
     val rootDiv = document.getElementById("root")
         render(rootDiv) {
-            gameUi()
+            gameUi(game)
 
     }
 }
