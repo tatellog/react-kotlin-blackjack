717f636553db27ab34f2d00758dbe8833313a61b
ttello <ttello@adquem.com>
2018-01-04 20:35:36 -0600

    style was added to handUi

--- src/blackjack/handUi.kt
+++ src/blackjack/handUi.kt
@@ -1,12 +1,26 @@
 package blackjack
 
 import blackjack.models.Hand
+import kotlinext.js.js
+import kotlinx.html.style
 import react.RBuilder
 import react.dom.b
 import react.dom.div
 
 fun RBuilder.handUi(hand: Hand) {
     div {
+
+        attrs.style = js {
+
+            width = "10rem"
+            height = "10rem"
+            padding = "1rem"
+            marginTop = "1rem"
+            marginRight = "1rem"
+            background = "cyan"
+
+        }
+
         div { b { +"Player Hand" } }
         div { b {
             hand.cards.forEach {
